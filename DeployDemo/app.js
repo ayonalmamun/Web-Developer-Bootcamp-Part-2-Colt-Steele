817b051e56f0067f21e11b523b29a09b3a47d6a1
root <ayonalmamun@gmail.com>
2020-05-17 07:38:25 +0000

    before deploying yelpcamp

---
+++ DeployDemo/app.js
@@ -0,0 +1,15 @@
+var express = require ("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+	console.log("Ayon Al Mamun Beta Started!");
+});
